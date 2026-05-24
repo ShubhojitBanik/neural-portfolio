5eeb1037da7801b5ae875051a1b17fabf85b2eec
Shubhojit Banik <sjbcs2023@gmail.com>
2026-05-24 15:02:08 +0600

    fixed app routing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
         setProjects(data)
 
       } catch (error) {
-        console.error(error)
+        console.error('API ERROR:', error)
       }
     }
 
@@ -32,7 +32,6 @@ function Home() {
 
       <Hero />
 
-      {/* PROJECTS */}
       <section className="max-w-6xl mx-auto px-8 py-24">
 
         <h2 className="text-5xl font-black mb-12">
@@ -41,32 +40,34 @@ function Home() {
 
         <div className="grid md:grid-cols-2 gap-8">
 
-          {projects.map((project) => (
-            <div
-              key={project._id}
-              className="p-8 rounded-3xl border border-white/10 bg-white/5"
-            >
-              <h3 className="text-3xl font-bold">
-                {project.title}
-              </h3>
+          {Array.isArray(projects) &&
+            projects.map((project) => (
 
-              <p className="mt-4 text-gray-400">
-                {project.description}
-              </p>
+              <div
+                key={project._id}
+                className="p-8 rounded-3xl border border-white/10 bg-white/5"
+              >
+                <h3 className="text-3xl font-bold">
+                  {project.title}
+                </h3>
 
-              <div className="flex gap-3 mt-6 flex-wrap">
+                <p className="mt-4 text-gray-400">
+                  {project.description}
+                </p>
 
-                {project.techStack.map((tech, index) => (
-                  <span
-                    key={index}
-                    className="px-3 py-1 rounded-lg bg-cyan-400/10 border border-cyan-400/20 text-cyan-300 text-sm"
-                  >
-                    {tech}
-                  </span>
-                ))}
+                <div className="flex gap-3 mt-6 flex-wrap">
+
+                  {project.techStack?.map((tech, index) => (
+                    <span
+                      key={index}
+                      className="px-3 py-1 rounded-lg bg-cyan-400/10 border border-cyan-400/20 text-cyan-300 text-sm"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </section>
     </div>
